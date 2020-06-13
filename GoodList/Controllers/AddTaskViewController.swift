664550058ab4c4aa93adac14bd0ae00fd5f7c43f
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-13 21:11:11 +0530

    Task subject published and subscribed using RxSwift

--- GoodList/Controllers/AddTaskViewController.swift
+++ GoodList/Controllers/AddTaskViewController.swift
@@ -8,12 +8,18 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 class AddTaskViewController: UIViewController {
     
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var taskTitleTextField: UITextField!
     
+    private let taskSubject = PublishSubject<Task>()
+    
+    var taskSubjectObservable: Observable<Task> {
+        return self.taskSubject.asObservable()
+    }
     @IBAction func saveTask() {
         
         guard let priority = Priority(rawValue: self.prioritySegmentedControl.selectedSegmentIndex), let title = self.taskTitleTextField.text else {
@@ -21,5 +27,7 @@ class AddTaskViewController: UIViewController {
         }
         
         let task = Task(title: title, priority: priority)
+        taskSubject.onNext(task)
+        self.dismiss(animated: true, completion: nil)
     }
 }
--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -8,15 +8,29 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 class TaskListViewController: UIViewController {
     
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController, let addTaskVC = navC.viewControllers.first as? AddTaskViewController else {
+            return
+        }
+        addTaskVC.taskSubjectObservable
+            .subscribe(onNext: { task in
+                print(task)
+            })
+        .disposed(by: disposeBag)
     }
     
     
