7961755a48927cf137ca497e4210fe3622e58cfb
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-13 15:29:20 +0530

    Added Task View controller, implemented dummy datasource methods

---
+++ GoodList/Controllers/TaskListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TaskListViewController.swift
+//  GoodList
+//
+//  Created by Anuranjan Bose on 13/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UIViewController {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+}
+
+extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        return cell
+    }
+}
