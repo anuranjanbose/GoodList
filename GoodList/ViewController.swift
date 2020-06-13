6071cfb7b10dca81c4f5c9a2aca882fe1948b31f
Anuranjan Bose <anuranjan.bose@tothenew.com>
2020-06-13 15:16:12 +0530

    Initial project setup

---
+++ GoodList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by Anuranjan Bose on 13/06/20.
+//  Copyright © 2020 Anuranjan Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
